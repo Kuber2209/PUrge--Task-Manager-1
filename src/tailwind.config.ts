cc7da50d88792d46abf098e43b8418315b3cf643
Kuber Bhatt <prepkub@gmail.com>
2025-09-11 08:25:56 +0000

    /*===== GOOGLE FONTS =====*/ @import url("https://fonts.googleapis.com/c

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Lexend', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
