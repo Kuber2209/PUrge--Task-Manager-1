dcd12afc2e1277688c003b6ee9916b56a092c60f
Kuber Bhatt <prepkub@gmail.com>
2025-09-12 20:50:03 +0000

    i want the notifications to come on my phone even when the site is not o

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,67 +1,40 @@
+// This file needs to be in the public directory
 
-// Import the Firebase app and messaging libraries.
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
+// Scripts for firebase and messaging
 importScripts('https://www.gstatic.com/firebasejs/10.12.3/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.3/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker with your project's sender ID.
-// This is required to receive notifications when the app is in the background or closed.
-// The config object will be dynamically replaced by a fetch to /__/firebase/init.json
-const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
-  measurementId: "REPLACE_WITH_YOUR_MEASUREMENT_ID"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
-  // Customize the notification here
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.png', // Make sure you have an icon in your public folder
-    data: {
-      url: payload.fcmOptions.link // The URL to open when the notification is clicked
-    }
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+// Fetch the Firebase config from our secure API endpoint
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/__/firebase/init.json')) {
+    event.respondWith(
+      fetch('/api/firebase-config')
+    );
+  }
 });
 
-self.addEventListener('notificationclick', (event) => {
-  console.log('[firebase-messaging-sw.js] Notification click received.', event);
-  
-  event.notification.close();
-  
-  const urlToOpen = event.notification.data.url;
+// This is the crucial part: it fetches the config and initializes Firebase
+fetch('/__/firebase/init.json').then(response => response.json()).then(firebaseConfig => {
+    firebase.initializeApp(firebaseConfig);
 
-  event.waitUntil(
-    clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true
-    }).then((clientList) => {
-      // If a window for the app is already open, focus it.
-      for (const client of clientList) {
-        if (client.url === '/' && 'focus' in client) {
-          return client.focus();
-        }
-      }
-      // Otherwise, open a new window.
-      if (clients.openWindow) {
-        return clients.openWindow(urlToOpen);
-      }
-    })
-  );
+    if (firebase.messaging.isSupported()) {
+        const messaging = firebase.messaging();
+
+        // This is the background message handler.
+        // It's called when the app is in the background or closed.
+        messaging.onBackgroundMessage((payload) => {
+            console.log(
+                '[firebase-messaging-sw.js] Received background message ',
+                payload
+            );
+            
+            const notificationTitle = payload.notification.title;
+            const notificationOptions = {
+                body: payload.notification.body,
+                icon: '/icon-192x192.png' // Make sure you have an icon in your public folder
+            };
+
+            self.registration.showNotification(notificationTitle, notificationOptions);
+        });
+    }
 });
